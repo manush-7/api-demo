c05a346166dfa67045bf3053aa93f5ed4b2260cf
Bansi Delwadia <58434120+BansiDelwadia@users.noreply.github.com>
2022-05-26 18:14:34 +0530

    File Addition
    Added .sql and .csv files of test data

---
+++ testdata.sql
@@ -0,0 +1,46 @@
+CREATE TABLE mytable(
+   Sales_Org       INTEGER  NOT NULL
+  ,Plant           INTEGER  NOT NULL
+  ,Storage_Loc     INTEGER  NOT NULL
+  ,Item_Code       VARCHAR(30)
+  ,DiviCode        VARCHAR(30)
+  ,Material        VARCHAR(10) NOT NULL
+  ,Cust_Material   INTEGER  NOT NULL
+  ,EANUPC          INTEGER  NOT NULL
+  ,Description     VARCHAR(10) NOT NULL
+  ,Unrestricted    NUMERIC(8,3) NOT NULL
+  ,RestrictedUse   NUMERIC(5,3) NOT NULL
+  ,Safety_stock    NUMERIC(5,3) NOT NULL
+  ,Minimum_Qty     NUMERIC(5,3) NOT NULL
+  ,Quantity_in_UnE VARCHAR(30)
+  ,MaterialGroup_1 VARCHAR(30)
+  ,MTORF           VARCHAR(30)
+  ,Pl_Deliv_Time   INTEGER  NOT NULL
+  ,Customer        VARCHAR(8) NOT NULL
+  ,Acct_at_cust    VARCHAR(30)
+  ,Amount          VARCHAR(30)
+  ,Currency_key    VARCHAR(30)
+  ,Posting_Date    INTEGER  NOT NULL
+  ,Order_Quantity  VARCHAR(30)
+  ,Created_On      VARCHAR(30)
+  ,Time            VARCHAR(30)
+  ,Material_Doc    VARCHAR(30)
+  ,Document_Date   VARCHAR(30)
+  ,Reference       VARCHAR(30)
+  ,Gross_Weight    VARCHAR(30)
+  ,Net_Weight      VARCHAR(30)
+  ,PRODH           VARCHAR(30)
+  ,MaterialGroup_3 VARCHAR(30)
+  ,Matfreight_grp  VARCHAR(30)
+  ,Total_RL_time   VARCHAR(30)
+);
+INSERT INTO mytable(Sales_Org,Plant,Storage_Loc,Item_Code,DiviCode,Material,Cust_Material,EANUPC,Description,Unrestricted,RestrictedUse,Safety_stock,Minimum_Qty,Quantity_in_UnE,MaterialGroup_1,MTORF,Pl_Deliv_Time,Customer,Acct_at_cust,Amount,Currency_key,Posting_Date,Order_Quantity,Created_On,Time,Material_Doc,Document_Date,Reference,Gross_Weight,Net_Weight,PRODH,MaterialGroup_3,Matfreight_grp,Total_RL_time) VALUES (3900,3900,0800,NULL,NULL,'1013451S01',950074768,4011558743208,'W 713/28',6.000,0.000,0.000,0.000,NULL,NULL,NULL,50,'AU117900',NULL,NULL,NULL,00000000,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL);
+INSERT INTO mytable(Sales_Org,Plant,Storage_Loc,Item_Code,DiviCode,Material,Cust_Material,EANUPC,Description,Unrestricted,RestrictedUse,Safety_stock,Minimum_Qty,Quantity_in_UnE,MaterialGroup_1,MTORF,Pl_Deliv_Time,Customer,Acct_at_cust,Amount,Currency_key,Posting_Date,Order_Quantity,Created_On,Time,Material_Doc,Document_Date,Reference,Gross_Weight,Net_Weight,PRODH,MaterialGroup_3,Matfreight_grp,Total_RL_time) VALUES (3900,3900,0800,NULL,NULL,'1013524S01',950074792,4011558700102,'W 77',18.000,0.000,0.000,0.000,NULL,NULL,NULL,50,'AU117900',NULL,NULL,NULL,00000000,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL);
+INSERT INTO mytable(Sales_Org,Plant,Storage_Loc,Item_Code,DiviCode,Material,Cust_Material,EANUPC,Description,Unrestricted,RestrictedUse,Safety_stock,Minimum_Qty,Quantity_in_UnE,MaterialGroup_1,MTORF,Pl_Deliv_Time,Customer,Acct_at_cust,Amount,Currency_key,Posting_Date,Order_Quantity,Created_On,Time,Material_Doc,Document_Date,Reference,Gross_Weight,Net_Weight,PRODH,MaterialGroup_3,Matfreight_grp,Total_RL_time) VALUES (3900,3900,0800,NULL,NULL,'1013789S01',950074904,4011558941000,'WK 713',2.000,0.000,0.000,0.000,NULL,NULL,NULL,62,'AU117900',NULL,NULL,NULL,00000000,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL);
+INSERT INTO mytable(Sales_Org,Plant,Storage_Loc,Item_Code,DiviCode,Material,Cust_Material,EANUPC,Description,Unrestricted,RestrictedUse,Safety_stock,Minimum_Qty,Quantity_in_UnE,MaterialGroup_1,MTORF,Pl_Deliv_Time,Customer,Acct_at_cust,Amount,Currency_key,Posting_Date,Order_Quantity,Created_On,Time,Material_Doc,Document_Date,Reference,Gross_Weight,Net_Weight,PRODH,MaterialGroup_3,Matfreight_grp,Total_RL_time) VALUES (3900,3900,0800,NULL,NULL,'1013827S01',950074839,4011558743307,'W 940/62',30.000,0.000,0.000,0.000,NULL,NULL,NULL,57,'AU117900',NULL,NULL,NULL,00000000,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL);
+INSERT INTO mytable(Sales_Org,Plant,Storage_Loc,Item_Code,DiviCode,Material,Cust_Material,EANUPC,Description,Unrestricted,RestrictedUse,Safety_stock,Minimum_Qty,Quantity_in_UnE,MaterialGroup_1,MTORF,Pl_Deliv_Time,Customer,Acct_at_cust,Amount,Currency_key,Posting_Date,Order_Quantity,Created_On,Time,Material_Doc,Document_Date,Reference,Gross_Weight,Net_Weight,PRODH,MaterialGroup_3,Matfreight_grp,Total_RL_time) VALUES (3900,3900,0800,NULL,NULL,'1013912S01',950074618,4011558296001,'HU 721/4 X',363.000,0.000,0.000,0.000,NULL,NULL,NULL,50,'AU117900',NULL,NULL,NULL,00000000,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL);
+INSERT INTO mytable(Sales_Org,Plant,Storage_Loc,Item_Code,DiviCode,Material,Cust_Material,EANUPC,Description,Unrestricted,RestrictedUse,Safety_stock,Minimum_Qty,Quantity_in_UnE,MaterialGroup_1,MTORF,Pl_Deliv_Time,Customer,Acct_at_cust,Amount,Currency_key,Posting_Date,Order_Quantity,Created_On,Time,Material_Doc,Document_Date,Reference,Gross_Weight,Net_Weight,PRODH,MaterialGroup_3,Matfreight_grp,Total_RL_time) VALUES (3900,3900,0800,NULL,NULL,'1014014S02',950074580,4011558296308,'HU 514 X',1394.000,0.000,0.000,0.000,NULL,NULL,NULL,50,'AU117900',NULL,NULL,NULL,00000000,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL);
+INSERT INTO mytable(Sales_Org,Plant,Storage_Loc,Item_Code,DiviCode,Material,Cust_Material,EANUPC,Description,Unrestricted,RestrictedUse,Safety_stock,Minimum_Qty,Quantity_in_UnE,MaterialGroup_1,MTORF,Pl_Deliv_Time,Customer,Acct_at_cust,Amount,Currency_key,Posting_Date,Order_Quantity,Created_On,Time,Material_Doc,Document_Date,Reference,Gross_Weight,Net_Weight,PRODH,MaterialGroup_3,Matfreight_grp,Total_RL_time) VALUES (3900,3900,0800,NULL,NULL,'1014065S01',950074827,4011558727505,'W 933/1',90.000,0.000,0.000,0.000,NULL,NULL,NULL,50,'AU117900',NULL,NULL,NULL,00000000,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL);
+INSERT INTO mytable(Sales_Org,Plant,Storage_Loc,Item_Code,DiviCode,Material,Cust_Material,EANUPC,Description,Unrestricted,RestrictedUse,Safety_stock,Minimum_Qty,Quantity_in_UnE,MaterialGroup_1,MTORF,Pl_Deliv_Time,Customer,Acct_at_cust,Amount,Currency_key,Posting_Date,Order_Quantity,Created_On,Time,Material_Doc,Document_Date,Reference,Gross_Weight,Net_Weight,PRODH,MaterialGroup_3,Matfreight_grp,Total_RL_time) VALUES (3900,3900,0800,NULL,NULL,'1014262S01',950075013,4011558743901,'ZR 700 X',33.000,0.000,0.000,0.000,NULL,NULL,NULL,50,'AU117900',NULL,NULL,NULL,00000000,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL);
+INSERT INTO mytable(Sales_Org,Plant,Storage_Loc,Item_Code,DiviCode,Material,Cust_Material,EANUPC,Description,Unrestricted,RestrictedUse,Safety_stock,Minimum_Qty,Quantity_in_UnE,MaterialGroup_1,MTORF,Pl_Deliv_Time,Customer,Acct_at_cust,Amount,Currency_key,Posting_Date,Order_Quantity,Created_On,Time,Material_Doc,Document_Date,Reference,Gross_Weight,Net_Weight,PRODH,MaterialGroup_3,Matfreight_grp,Total_RL_time) VALUES (3900,3900,0800,NULL,NULL,'1014266S01',950075015,4011558743703,'ZR 903 X',12.000,0.000,0.000,0.000,NULL,NULL,NULL,50,'AU117900',NULL,NULL,NULL,00000000,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL);
+INSERT INTO mytable(Sales_Org,Plant,Storage_Loc,Item_Code,DiviCode,Material,Cust_Material,EANUPC,Description,Unrestricted,RestrictedUse,Safety_stock,Minimum_Qty,Quantity_in_UnE,MaterialGroup_1,MTORF,Pl_Deliv_Time,Customer,Acct_at_cust,Amount,Currency_key,Posting_Date,Order_Quantity,Created_On,Time,Material_Doc,Document_Date,Reference,Gross_Weight,Net_Weight,PRODH,MaterialGroup_3,Matfreight_grp,Total_RL_time) VALUES (3900,3900,0800,NULL,NULL,'1014268S01',950075016,4011558743802,'ZR 904 X',3.000,0.000,0.000,0.000,NULL,NULL,NULL,50,'AU117900',NULL,NULL,NULL,00000000,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL);
